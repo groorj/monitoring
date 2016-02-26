e3380e285f3ef97e6aec9e2ea773167e4ff2c363
Fernando Teixeira <fecotex@gmail.com>
2016-02-26 18:27:28 -0300

    Correcao de argumento no exemplo e adicao de verificacao de CN X dominio

--- ssl_certificate/check_cert_expiration_date.sh
+++ ssl_certificate/check_cert_expiration_date.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #########################################################################################
-# Date:		Sat 30 Jan 2016 19:10:39 EST
-# Version:	1.0.2
-# Author:	gustavo.aguiar@gmail.com
-# Github:	https://github.com/groorj/monitoring/
-# LastChange:	Sat 30 Jan 2016 19:51:09 EST	<gustavo.aguiar@gmail.com>	<1.0.0>
-# LastChange:	Sun 31 Jan 2016 11:47:46 EST	<gustavo.aguiar@gmail.com>	<1.0.1>
-# LastChange:	Sun 31 Jan 2016 12:27:15 EST	<gustavo.aguiar@gmail.com>	<1.0.2>
-# Request:	N/A
-# Description:	Shell script to validate if ssl cert is about to expire
-# Usage:	./check_cert_expiration_date.sh -d <domain_name> -c <days_prior_expire>
+# Date:         Sat 30 Jan 2016 19:10:39 EST
+# Version:      1.0.3
+# Author:       gustavo.aguiar@gmail.com
+# Github:       https://github.com/groorj/monitoring/
+# LastChange:   Sat 30 Jan 2016 19:51:09 EST    <gustavo.aguiar@gmail.com>      <1.0.0>
+# LastChange:   Sun 31 Jan 2016 11:47:46 EST    <gustavo.aguiar@gmail.com>      <1.0.1>
+# LastChange:   Sun 31 Jan 2016 12:27:15 EST    <gustavo.aguiar@gmail.com>      <1.0.2>
+# LastChange:   Fri Feb 26 18:22:36 BRT 2016    <fecotex@gmail.com>     	<1.0.3>
+# Request:      N/A
+# Description:  Shell script to validate if ssl cert is about to expire
+# Usage:        ./check_cert_expiration_date.sh -d <domain_name> -c <days_prior_expire>
 # Example:
-#		./check_cert_expiration_date.sh -d google.com -c 30
-# Return:	OK|ERROR
+#               ./check_cert_expiration_date.sh -d google.com -c 30
+# Return:       OK|ERROR
 #########################################################################################
 
 #########################################################################################
@@ -49,29 +50,38 @@ convert_to_mjd() {
 
 # print verbose information ;
 function echo_verbose {
-	if [ $VERBOSE ]; then
-		echo "[`date '+%Y-%m-%d %H:%M:%S'`] - ${*}"
-	fi
+        if [ $VERBOSE ]; then
+                echo "[`date '+%Y-%m-%d %H:%M:%S'`] - ${*}"
+        fi
 }
 
 
 # print usage menu ;
 function usage {
-	echo ""
-	echo "$(basename "$0") [-h] [-v] [-d] [-c] [-p] -- check ssl certificate for expiration information."
-	echo ""
-	echo "Usage:"
-	printf "    $0 -h ${RED}->${NC} show this help text\n"
-	printf "    $0 -v ${RED}->${NC} verbose\n"
-	printf "    $0 -d ${RED}->${NC} domain to check (required)\n"
-	printf "    $0 -c ${RED}->${NC} days before expire critical alarm (required)\n"
-	printf "    $0 -p ${RED}->${NC} port (optional :: default is 443)\n"
-	echo "Example:"
-	echo "    $0 -d google.com -e 30"
-	echo ""
-	exit 0
+        echo ""
+        echo "$(basename "$0") [-h] [-v] [-d] [-c] [-p] -- check ssl certificate for expiration information."
+        echo ""
+        echo "Usage:"
+        printf "    $0 -h ${RED}->${NC} show this help text\n"
+        printf "    $0 -v ${RED}->${NC} verbose\n"
+        printf "    $0 -d ${RED}->${NC} domain to check (required)\n"
+        printf "    $0 -c ${RED}->${NC} days before expire critical alarm (required)\n"
+        printf "    $0 -p ${RED}->${NC} port (optional :: default is 443)\n"
+        echo "Example:"
+        echo "    $0 -d google.com -c 30"
+        echo ""
+        exit 0
 }
  
+function valid_neg {
+
+        if [ $? -eq 1 ]; then
+                echo ERROR
+                exit 1
+        fi
+
+}
+
 
 #########################################################################################
 # Main code
@@ -80,38 +90,38 @@ function usage {
 # no arguments provided ;
 if [ $# -eq 0 ]; then
         usage;
-	exit 1
+        exit 1
 fi
 
 # check for provided arguments ;
 while getopts ':hvp:d:c:' option; do
   case $option in
     h)
-	usage;
-	exit 0
-	;;
+        usage;
+        exit 0
+        ;;
     v)
-	VERBOSE=true
-	;;
+        VERBOSE=true
+        ;;
     p)
-	PORT=$OPTARG
-	;;
+        PORT=$OPTARG
+        ;;
     d)
-	DOMAIN=$OPTARG
-	;;
+        DOMAIN=$OPTARG
+        ;;
     c)
-	DAYS_TO_ALERT_CRITC=$OPTARG
-	;;
+        DAYS_TO_ALERT_CRITC=$OPTARG
+        ;;
    \?)
-	printf "\n${RED}Illegal option: -%s${NC}\n" "$OPTARG" >&2
-	usage;
-	exit 1
-	;;
+        printf "\n${RED}Illegal option: -%s${NC}\n" "$OPTARG" >&2
+        usage;
+        exit 1
+        ;;
     :)
-	printf "\n${RED}Missing argument for -%s${NC}\n" "$OPTARG" >&2
-	usage;
-	exit 1
-	;;
+        printf "\n${RED}Missing argument for -%s${NC}\n" "$OPTARG" >&2
+        usage;
+        exit 1
+        ;;
   esac
 done
 
@@ -122,17 +132,23 @@ echo_verbose "DAYS_TO_ALERT_CRITC: $DAYS_TO_ALERT_CRITC"
 # check if -d and -c options are present ;
 if [ -z $DOMAIN ] || [ -z $DAYS_TO_ALERT_CRITC ]; then
         printf "\n${RED}Options -d and -c are required${NC}\n" "$OPTARG" >&2
-	usage;
-	exit 1
+        usage;
+        exit 1
 fi
 
 # check for SSL port ;
 echo_verbose "PORT WAS: $PORT"
 if [ -z $PORT ]; then
-	echo_verbose "No port was provided lets use default port [$DEFAULT_PORT]."
-	PORT=$DEFAULT_PORT
+        echo_verbose "No port was provided lets use default port [$DEFAULT_PORT]."
+        PORT=$DEFAULT_PORT
 fi
 echo_verbose "PORT: $PORT"
+
+# verify cert-cn X domain
+CN_FROM_CERT=`echo | openssl s_client -connect $DOMAIN:$PORT 2>/dev/null | openssl x509 -noout -subject -nameopt multiline | awk '/commonName/ {print$NF}'`
+
+(echo "$DOMAIN" | grep -Eq  ^$CN_FROM_CERT$)
+valid_neg
 
 # retrieve the expiration date from the SSL certificate using the openssl tool ;
 DATE_FROM_CERT=`echo | openssl s_client -connect $DOMAIN:$PORT 2>/dev/null | openssl x509 -noout -dates | tail -1 | tr -s " " |cut -d "=" -f2 | cut -d " " -f1,2,4`
@@ -166,11 +182,11 @@ echo_verbose "DATE_DIFF: $DATE_DIFF"
 
 # check if the result from the calulation is greater then the specified days to alarm ;
 if [[ $DATE_DIFF -gt $DAYS_TO_ALERT_CRITC ]]; then
-	echo "OK"
-	exit 0
+        echo "OK"
+        exit 0
 else
-	echo "ERROR"
-	exit 1
+        echo "ERROR"
+        exit 1
 fi
 
 
